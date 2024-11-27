b1638bedd94f141e3d461b8d0079f3445dac35b6
Muhammad Umair <121006407+m-umairshah@users.noreply.github.com>
2024-11-27 15:34:08 +0500

    Update run.py

--- run.py
+++ run.py
@@ -4,5 +4,5 @@ app = create_app()
 
 if __name__ == "__main__":
     with app.app_context():
-        db.create_all()  # Ensure database tables are created
+        db.create_all() 
     app.run(debug=True)
